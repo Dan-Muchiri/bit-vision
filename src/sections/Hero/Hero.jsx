d8bfd9bcf7e563cd616be15e27bb59fd98abec15
Dan-Muchiri <danspmunene@gmail.com>
2024-10-09 16:02:47 +0300

    Created hero background

---
+++ src/sections/Hero/Hero.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import styles from "./HeroStyles.module.css";
+import backimage from "../../../public/back.png";
+import backimagesmall from "../../../public/back small.png";
+
+function Hero() {
+  return (
+    <div className={styles.container}>
+      <section id='hero' className={styles.sect}>
+        <div className={styles.back}>
+          <div className={styles.backImage}>
+            <img src={backimage} className={styles.image} alt="Background" />
+            <img src={backimagesmall} className={styles.imageSmall} alt="Small Background" />
+            <div className={styles.overlay} /> {/* Background overlay */}
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default Hero;
