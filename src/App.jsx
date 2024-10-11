2082cba4158c7edb0d18f56c39d5ed1446766ca7
Dan-Muchiri <danspmunene@gmail.com>
2024-10-11 14:36:16 +0300

    Added right side to services

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Hero from './sections/Hero/Hero'
 import Header from './sections/Header/Header'
+import Services from './sections/Services/Services';
 
 function App() {
  
@@ -11,6 +12,7 @@ function App() {
     <Router>
       <Header/>
       <Hero/>
+      <Services/>
     </Router>  
     </>
   )
---
+++ src/sections/Services/Services.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styles from "./ServicesStyles.module.css"
+
+function Services() {
+  return (
+    <div className={styles.rightWords}>
+              <p className={styles.firstP}>[/bɪt vɪʒən/]noun</p>
+              <p className={styles.middleP}>We are Bit Vision, an award-winning Marketing and Web Design Agency that will offer you transparency and creativity.</p>
+              <p className={styles.lastP}>
+                Our goal is to nurture your vision and provide innovative, custom solutions for all your marketing needs. We provide a range of customized services, including branding, website design & development, SEO, marketing consultation, and more.
+              </p>
+            </div>
+  )
+}
+
+export default Services
